fcc7636036ba48713075ece63c122b93286c1c39
Lak <Thichmonney05@gmail.com>
2026-09-11 17:31:54 +0700

    Add Gen Qr code

---
+++ src/main/java/com/sonchau/utilities/controller/qr/QrRestController.java
@@ -0,0 +1,33 @@
+package com.sonchau.utilities.controller.qr;
+
+import com.sonchau.utilities.service.qr.QrService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QrRestController {
+
+    private final QrService qrService;
+
+    public QrRestController(QrService qrService) {
+        this.qrService = qrService;
+    }
+
+    @GetMapping("/api/qr/generate")
+    public ResponseEntity<byte[]> generateQrCode(@RequestParam("data") String data) {
+        try {
+            byte[] imageBytes = qrService.generateQrCodeImage(data, 400, 400);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
